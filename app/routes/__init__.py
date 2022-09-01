0b4e13a0e5ace188a311af395ed5e46b553d0d6d
BakungaBronson <bakungabro@gmail.com>
2022-09-01 14:08:40 +0300

    Added status route

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,7 +1,6 @@
 from flask_restful import Api
 from app.controllers import (
-    IndexView, UserView, UserDetailView, UserLoginView, ResetPassword,
-    ProductView, ProductDetailView)
+    IndexView, PhysicalClusterStatusView)
 
 
 api = Api()
@@ -10,14 +9,5 @@ api = Api()
 api.add_resource(IndexView, '/')
 
 # User routes
-api.add_resource(UserView, '/users', endpoint='users')
-api.add_resource(UserDetailView, '/users/<string:user_id>',
-                 endpoint='user')
-api.add_resource(UserLoginView, '/users/login', endpoint='user_login')
-api.add_resource(ResetPassword, '/users/reset_password',
-                 endpoint='reset_password')
+api.add_resource(PhysicalClusterStatusView, '/status', endpoint='physical_cluster_status')
 
-# Product routes
-api.add_resource(ProductView, '/products', endpoint='products')
-api.add_resource(ProductDetailView, '/products/<string:product_id>',
-                 endpoint='product')
