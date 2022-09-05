9236cb9b8e2f1fe51a0de849a8f17926adde6c80
BakungaBronson <bakungabro@gmail.com>
2022-09-05 09:03:53 +0300

    Added cluster log data route

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,6 +1,6 @@
 from flask_restful import Api
 from app.controllers import (
-    IndexView, PhysicalClusterStatusView)
+    IndexView, PhysicalClusterStatusView, PhysicalClusterInfo)
 
 
 api = Api()
@@ -10,4 +10,5 @@ api.add_resource(IndexView, '/')
 
 # User routes
 api.add_resource(PhysicalClusterStatusView, '/status', endpoint='physical_cluster_status')
+api.add_resource(PhysicalClusterInfo, '/status/cluster_data')
 
