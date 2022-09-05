082d52337311e9dba20b5ff8b5a4503a9292fe17
BakungaBronson <bakungabro@gmail.com>
2022-09-05 06:45:43 +0300

    Added ClusterLogsSchema

---
+++ app/schemas/logs.py
@@ -0,0 +1,8 @@
+from marshmallow import Schema, fields
+
+class ClusterLogsSchema(Schema):
+
+    id = fields.UUID(dump_only=True)
+    cluster_id = fields.String()
+    status = fields.String()
+    date_created = fields.Date(dump_only=True)
