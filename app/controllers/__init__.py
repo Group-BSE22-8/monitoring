0617197ba227df5649d7f1d80dfc6c06a9a36fbb
BakungaBronson <bakungabro@gmail.com>
2022-09-08 22:58:24 +0300

    Added sendMail controller

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,3 +1,4 @@
 from .index import IndexView
 from .physical_cluster_status import PhysicalClusterStatusView, PhysicalClusterInfo
 from .proxmox_info import ProxmoxClusterInfoView
+from .send_mail import SendMail
